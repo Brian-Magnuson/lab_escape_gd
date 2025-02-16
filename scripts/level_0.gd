6773b6ce3bb0b1bef5e29424e90d0fa76c191635
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2025-02-16 10:45:29 -0500

    Add dynamic health bar text

--- scripts/level_0.gd
+++ scripts/level_0.gd
@@ -2,3 +2,4 @@ extends Node
 
 func _on_player_health_updated(health: float, max_health: float) -> void:
 	$CanvasLayer/HealthBar.value = health / max_health
+	$CanvasLayer/HealthBar/Label.text = "%0.0f / %0.0f" % [health, max_health]
