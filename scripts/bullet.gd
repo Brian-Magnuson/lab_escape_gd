b40e80d347c22cce4d241db3290f0b7226b9faab
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2025-02-24 09:06:07 -0500

    Add destructible bullet

---
+++ scripts/bullet.gd
@@ -0,0 +1,9 @@
+extends StaticBody2D
+
+@export var damage = 10.0
+
+func _ready() -> void:
+	$Hitbox.set_meta("hit_damage", damage)
+
+func _on_hitbox_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	queue_free()
