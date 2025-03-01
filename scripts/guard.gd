cdfe7ff29e289069ca72e5c7dec0d04fcc48576d
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2025-03-01 10:47:16 -0500

    Add bullet shooting

--- scripts/guard.gd
+++ scripts/guard.gd
@@ -7,7 +7,9 @@ extends Path2D
 @export var speed = 40.0
 
 @export var player: Node2D
+
 @export var bullet: PackedScene
+@export var bullet_speed = 50
 
 func _ready() -> void:
 	$PathFollow2D/Hitbox.set_meta("hit_damage", damage)
@@ -30,6 +32,14 @@ func hit(amount: float) -> void:
 	
 	if health == 0.0:
 		queue_free()
+		
+func shoot_bullet() -> void:
+	var bullet_inst = bullet.instantiate() as RigidBody2D
+	var v1 = $PathFollow2D.global_position
+	var v2 = player.global_position
+	bullet_inst.position = v1 - 20 * (v1 - v2).normalized()
+	bullet_inst.linear_velocity = bullet_speed * (v2 - v1).normalized()
+	get_tree().root.add_child(bullet_inst)
 	
 func update_health_bar() -> void:
 	$PathFollow2D/HealthBarAnchor/HealthBar.value = health / max_health
@@ -48,18 +58,17 @@ func _on_i_frame_timer_timeout() -> void:
 		if area.is_in_group("player_hitbox"):
 			hit(area.get_meta("hit_damage") as float)
 
-
 func _on_detection_zone_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		speed = 0
 		$PathFollow2D/AnimatedSprite2D.play("shooting")
-		var bullet_inst = bullet.instantiate() as Node2D
-		var v1 = $PathFollow2D.global_position
-		var v2 = player.global_position
-		bullet_inst.position = v1 - 20 * (v1 - v2).normalized()
-		get_tree().root.add_child(bullet_inst)
+		$PathFollow2D/ShootTimer.start()
 		
 func _on_detection_zone_body_exited(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		speed = 40.0
 		$PathFollow2D/AnimatedSprite2D.play("walking")
+		$PathFollow2D/ShootTimer.stop()
+
+func _on_shoot_timer_timeout() -> void:
+	shoot_bullet()
