8ac3817786aa3701f157745c645e9ebb23c6319d
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2025-03-08 10:57:21 -0500

    Change UI to be controlled by UI node

---
+++ scripts/user_interface.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+var score = 0.0
+
+func _ready() -> void:
+	$ColorRect.visible = true
+
+func _on_player_health_updated(health: float, max_health: float) -> void:
+	$HealthBar.value = health / max_health
+	$HealthBar/Label.text = "%0.0f / %0.0f" % [health, max_health]
+
+func _on_player_score_updated(amount: float) -> void:
+	score += amount
+	$ScoreLabel.text = "%0.0f" % score
+
+func _on_player_player_died() -> void:
+	await get_tree().create_timer(4.0).timeout
+	$ColorRect/AnimationPlayer.play("fade_out")
