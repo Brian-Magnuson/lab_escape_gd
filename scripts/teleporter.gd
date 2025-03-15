aaad96bed3d1f06c45f50c4822736e539e6e1357
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2025-03-15 09:59:49 -0400

    Add teleporter pair

--- scripts/teleporter.gd
+++ scripts/teleporter.gd
@@ -5,4 +5,4 @@ extends StaticBody2D
 func _physics_process(_delta: float) -> void:
 	for body in $TeleportRegion.get_overlapping_bodies():
 		if body.is_in_group("player") and Input.is_action_just_pressed("interact"):
-			body.position = teleport_target.position
+			body.global_position = teleport_target.global_position
