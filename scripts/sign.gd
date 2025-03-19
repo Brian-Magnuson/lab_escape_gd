8d985161b1c963dcf53a3adaef23f2d60499bb4a
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2025-03-19 10:14:53 -0400

    Add basic dialogue system

--- scripts/sign.gd
+++ scripts/sign.gd
@@ -1,6 +1,10 @@
 extends Area2D
 
+signal sign_read(dialogue_id: String)
+
+@export var dialogue_id: String = "Default";
+
 func _physics_process(_delta: float) -> void:
 	for body in get_overlapping_bodies():
 		if body.is_in_group("player") and Input.is_action_just_pressed("interact"):
-			print("Sign checked")
+			sign_read.emit(dialogue_id)
--- scripts/user_interface.gd
+++ scripts/user_interface.gd
@@ -2,8 +2,14 @@ extends CanvasLayer
 
 var score = 0.0
 
+var dialogue_data: Dictionary
+var current_dialogue: Dictionary
+var current_dialogue_index = -1
+
 func _ready() -> void:
 	$BlackScreen/AnimationPlayer.play("fade_in")
+	var dialogue_json_text = FileAccess.get_file_as_string("res://data/dialogue.json")
+	dialogue_data = JSON.parse_string(dialogue_json_text)
 
 func _on_player_health_updated(health: float, max_health: float) -> void:
 	$HealthBar.value = health / max_health
@@ -16,3 +22,27 @@ func _on_player_score_updated(amount: float) -> void:
 func _on_player_player_died() -> void:
 	await get_tree().create_timer(2.0).timeout
 	$BlackScreen/AnimationPlayer.play("fade_out")
+
+func find_dialogue_with_id(dialogue_id: String) -> Dictionary:
+	for obj in dialogue_data["dialogues"]:
+		if obj["id"] == dialogue_id:
+			return obj
+	return {}
+
+func _on_sign_sign_read(dialogue_id: String) -> void:
+	if current_dialogue.is_empty():
+		current_dialogue = find_dialogue_with_id(dialogue_id)
+		if current_dialogue.is_empty():
+			printerr("%s: Dialogue failed to load" % name)
+			return
+		current_dialogue_index = 0
+		$TextBox.visible = true
+		$TextBox/Label.text = current_dialogue["dialogue"][current_dialogue_index]["text"]
+	elif current_dialogue_index + 1 < current_dialogue["dialogue"].size():
+		current_dialogue_index += 1
+		$TextBox/Label.text = current_dialogue["dialogue"][current_dialogue_index]["text"]
+	else:
+		current_dialogue_index = -1
+		current_dialogue = {}
+		$TextBox.visible = false
+		
