9d3fbaad165b37bb665598f5cf44d21af32cbfd4
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2025-03-20 10:17:16 -0400

    Change sign interaction to be more efficient

--- scripts/sign.gd
+++ scripts/sign.gd
@@ -2,9 +2,18 @@ extends Area2D
 
 signal sign_read(dialogue_id: String)
 
-@export var dialogue_id: String = "Default";
+@export var dialogue_id: String = "Default"
 
-func _physics_process(_delta: float) -> void:
-	for body in get_overlapping_bodies():
-		if body.is_in_group("player") and Input.is_action_just_pressed("interact"):
-			sign_read.emit(dialogue_id)
+var player_in_range = false
+
+func _on_body_entered(body: Node) -> void:
+	if body.is_in_group("player"):
+		player_in_range = true
+
+func _on_body_exited(body: Node) -> void:
+	if body.is_in_group("player"):
+		player_in_range = false
+		
+func _input(event: InputEvent) -> void:
+	if player_in_range and event.is_action_pressed("interact"):
+		sign_read.emit(dialogue_id)
