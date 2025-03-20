0b845f9db447b76662ef340a49abb00ea9f39b25
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2025-03-20 10:22:39 -0400

    Change teleporter interaction to be more efficient

--- scripts/teleporter.gd
+++ scripts/teleporter.gd
@@ -1,8 +1,19 @@
 extends StaticBody2D
 
-@export var teleport_target: Node2D;
+@export var teleport_target: Node2D
 
-func _physics_process(_delta: float) -> void:
-	for body in $TeleportRegion.get_overlapping_bodies():
-		if body.is_in_group("player") and Input.is_action_just_pressed("interact"):
-			body.global_position = teleport_target.global_position
+var player_in_range = false
+var player_body: PhysicsBody2D
+
+func _on_teleport_region_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player_in_range = true
+		player_body = body
+
+func _on_teleport_region_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		player_in_range = false
+
+func _input(event: InputEvent) -> void:
+	if player_in_range and event.is_action_pressed("interact"):
+		player_body.global_position = teleport_target.global_position
